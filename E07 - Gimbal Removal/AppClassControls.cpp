6e0d15b02e01a79682b7253a4fe85b88b6c53269
JLeBerth <42814446+JLeBerth@users.noreply.github.com>
2019-09-30 09:23:08 -0400

    Completed Exercise 7
    Did Exercize 7

--- E07 - Gimbal Removal/AppClassControls.cpp
+++ E07 - Gimbal Removal/AppClassControls.cpp
@@ -417,23 +417,41 @@ void Application::ProcessKeyboard(void)
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::X))
 	{
 		if (fMultiplier)
-			m_v3Rotation.x -= 1.0f;
+		{
+			quaternion rotationQuat = glm::angleAxis(glm::radians(1.0f), vector3(-1.0f, 0.0f, 0.0f));
+			m_qOrientation = m_qOrientation * rotationQuat;
+		}
 		else
-			m_v3Rotation.x += 1.0f;
+		{
+			quaternion rotationQuat = glm::angleAxis(glm::radians(1.0f), vector3(1.0f, 0.0f, 0.0f));
+			m_qOrientation = m_qOrientation * rotationQuat;
+		}
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Y))
 	{
 		if (fMultiplier)
-			m_v3Rotation.y -= 1.0f;
+		{
+			quaternion rotationQuat = glm::angleAxis(glm::radians(1.0f), vector3(0.0f, -1.0f, 0.0f));
+			m_qOrientation = m_qOrientation * rotationQuat;
+		}
 		else
-			m_v3Rotation.y += 1.0f;
+		{
+			quaternion rotationQuat = glm::angleAxis(glm::radians(1.0f), vector3(0.0f, 1.0f, 0.0f));
+			m_qOrientation = m_qOrientation * rotationQuat;
+		}
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
 	{
 		if (fMultiplier)
-			m_v3Rotation.z -= 1.0f;
+		{
+			quaternion rotationQuat = glm::angleAxis(glm::radians(1.0f), vector3(0.0f, 0.0f, -1.0f));
+			m_qOrientation = m_qOrientation * rotationQuat;
+		}
 		else
-			m_v3Rotation.z += 1.0f;
+		{
+			quaternion rotationQuat = glm::angleAxis(glm::radians(1.0f), vector3(0.0f, 0.0f, 1.0f));
+			m_qOrientation = m_qOrientation * rotationQuat;
+		}
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::R))
 	{
